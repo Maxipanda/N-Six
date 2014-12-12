cf71fc5ecfee2387a050a431e7d1694b237717e2
Maxipanda <guimeunier_8@hotmail.com>
2014-12-12 09:21:15 +0100

    initialisation after body load

--- js/display.js
+++ js/display.js
@@ -1,1 +1,22 @@
 ﻿
+function initialisation() {
+    var gameCanvas = document.createElement("canvas");
+    gameCanvas.setAttribute("id", "gameCanvas");
+    document.getElementById("content").appendChild(gameCanvas);
+    console.log("End of initialisation...")
+}
+
+function mainLoop() {
+
+
+}
+
+function updatePhase() {
+
+
+}
+
+function renderPhase() {
+
+
+}
