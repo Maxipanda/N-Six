3f57265d31d1a34fac23fada25d83100633ac605
Ketero <k.bottero@live.fr>
2014-12-14 16:06:11 +0100

    GameEntity.js

---
+++ js/GameEntity.js
@@ -0,0 +1,44 @@
+/**
+* @class GameEntity
+*
+* @param x Coordinate x
+* @param y Coordinate y
+* @param z Coordinate z
+* @param collisionGroups List of the different collision groups of the entity
+* @param collisionFilters List of the different collision filters of the object
+*
+*/
+function GameEntity(x,y,z,collisionGroups,collisionFilters){
+    this.x = x,
+    this.y = y,
+    this.z = z,
+    this.collisionGroups = collisionGroups,
+    this.collisionFilters = collisionFilters,
+	
+    this.canCollideWith = function(e){
+		if(e == undefined)
+		{
+			alert("Function canCollideWith undefined e (parameter GameEntity)");
+		}
+        return false;
+    }
+	
+    this.update = function(){
+        
+    }
+	
+    this.render = function(g){
+        if(g == undefined)
+		{
+			alert("Function render undefined parameter g (HTMLCanvas2DContext)");
+		}
+    }
+	
+    this.hitTest = function(entity){
+		if(entity == undefined)
+		{
+			alert("Function hitTest undefined parameter entity (GameEntity)");
+		}
+        return false;
+    }
+}
